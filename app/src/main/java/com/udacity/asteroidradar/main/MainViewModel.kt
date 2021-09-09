bbf0a469717d902e823a07c66504de2e6c40c24e
Ryan Wong <ryan@rwmobi.com>
2021-09-09 21:28:46 +0100

    [feature] Main: implement MainViewModel and ViewModelFactory

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,22 @@
 package com.udacity.asteroidradar.main
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Repository.AsteroidRepository
+import com.udacity.asteroidradar.database.getDatabase
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : ViewModel() {
+
+    private val database = getDatabase(application)
+    private val asteroidRepository = AsteroidRepository(database)
+    val asteroidList = asteroidRepository.asteroids
+
+    init {
+        viewModelScope.launch {
+            asteroidRepository.refreshAsteroids()
+        }
+    }
+
 }
