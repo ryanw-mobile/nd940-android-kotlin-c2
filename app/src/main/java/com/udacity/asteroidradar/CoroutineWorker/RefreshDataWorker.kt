21e04752abdcd3b2638c6296fd3946385021432f
Ryan Wong <ryan@rwmobi.com>
2021-09-09 21:32:10 +0100

    [feature] App: add WorkManager and Coroutine Worker class

---
+++ app/src/main/java/com/udacity/asteroidradar/CoroutineWorker/RefreshDataWorker.kt
@@ -0,0 +1,28 @@
+package com.udacity.asteroidradar.CoroutineWorker
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.udacity.asteroidradar.Repository.AsteroidRepository
+import com.udacity.asteroidradar.database.getDatabase
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = AsteroidRepository(database)
+
+        return try {
+            repository.refreshAsteroids()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+}
