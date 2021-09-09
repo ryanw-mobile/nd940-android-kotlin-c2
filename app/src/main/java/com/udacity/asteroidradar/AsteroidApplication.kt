18059c84a9e51cb67e3a4ba993de8689a14797be
Ryan Wong <ryan@rwmobi.com>
2021-09-10 00:02:22 +0100

    [feature] App: update the constraints to trigger WorkManager automatically download today's asteroid under the given criteria

--- app/src/main/java/com/udacity/asteroidradar/AsteroidApplication.kt
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidApplication.kt
@@ -1,7 +1,6 @@
 package com.udacity.asteroidradar
 
 import android.app.Application
-import android.os.Build
 import androidx.work.*
 import com.udacity.asteroidradar.CoroutineWorker.RefreshDataWorker
 import kotlinx.coroutines.CoroutineScope
@@ -12,7 +11,7 @@ import java.util.concurrent.TimeUnit
 
 class AsteroidApplication : Application() {
 
-    val applicationScope = CoroutineScope(Dispatchers.Default)
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreate() {
         super.onCreate()
@@ -25,16 +24,16 @@ class AsteroidApplication : Application() {
         setupRecurringWork()
     }
 
+    /**
+     * Project requirement:
+     * Be able to cache the data of the asteroid by using a worker, so it downloads and saves
+     * today's asteroids in background once a day when the device is charging and wifi is enabled.
+     */
     private fun setupRecurringWork() {
         val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.UNMETERED)
-            .setRequiresBatteryNotLow(true)
+            .setRequiredNetworkType(NetworkType.UNMETERED) // I guess this should refer to WIFI
             .setRequiresCharging(true)
-            .apply {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    setRequiresDeviceIdle(true)
-                }
-            }.build()
+            .build()
 
         val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
             .setConstraints(constraints)
@@ -42,7 +41,7 @@ class AsteroidApplication : Application() {
 
         WorkManager.getInstance(this).enqueueUniquePeriodicWork(
             RefreshDataWorker.WORK_NAME,
-            ExistingPeriodicWorkPolicy.KEEP, // Keep old pending request if exists, and do nothing
+            ExistingPeriodicWorkPolicy.KEEP, // Keep old pending job if exists, and not adding this job
             repeatingRequest
         )
     }
